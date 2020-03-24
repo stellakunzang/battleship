c3dffed411365ca24bb0f64bcab25e026daf4b2d
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-24 16:30:31 -0600

    Removed comments

--- lib/cell.rb
+++ lib/cell.rb
@@ -5,16 +5,8 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @empty = true
-    @ship = ship
+    @ship = nil
     @fired_upon = false
-  end
-
-  def ship
-    if @empty == true
-      nil
-    else
-      @ship
-    end
   end
 
   def empty?
@@ -29,11 +21,12 @@ class Cell
   def fired_upon?
     @fired_upon
   end
-    # not yet sure that the fire_upon method can change the value or if that requires creating an instance variable
 
   def fire_upon
     @fired_upon = true
-    ship.health -= 1
+    if @ship == ship
+      ship.hit
+    end
   end
 
   def render(show = false)
@@ -43,16 +36,14 @@ class Cell
       else
         "."
       end
-    elsif @fired_upon == true && ship.health == ship.length
-      "M"
-    elsif @fired_upon == true && ship.length > ship.health < 0
-      # not sure if this combination of conditionals will work
-      "H"
-    elsif @fired_upon == true && ship.sunk? == true
-      "X"
+    elsif @fired_upon == true
+      if ship.health == ship.length
+        "M"
+      elsif ship.length > ship.health && ship.sunk? == false
+        "H"
+      elsif ship.sunk? == true
+        "X"
+      end
     end
   end
 end
-    # it takes an optional boolean argument but I don't understand why
-    # this method displays whether the cell has been fired upon, whether it contains a ship, etc.
-    #
