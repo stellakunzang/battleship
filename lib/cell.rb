0e5de8f1e70a51e2e4aa122f034e66407a51d849
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-24 16:24:39 -0600

    Made new instance variables because having trouble integrating changes from ship class

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,17 +1,19 @@
 class Cell
 
-  attr_reader :coordinate, :empty, :ship
+  attr_reader :coordinate, :empty, :ship, :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
     @empty = true
+    @ship = ship
+    @fired_upon = false
   end
 
   def ship
     if @empty == true
       nil
     else
-      ship
+      @ship
     end
   end
 
@@ -21,41 +23,36 @@ class Cell
 
   def place_ship(ship)
     @empty = false
-    ship = ship
+    @ship = ship
   end
 
   def fired_upon?
-    false
+    @fired_upon
   end
     # not yet sure that the fire_upon method can change the value or if that requires creating an instance variable
 
   def fire_upon
-    fired_upon? == true
-    if ship
-      ship.health -= 1
-      # will we need to add to this method for sunken ship?
-      # can use ship.hit here?
-    end
+    @fired_upon = true
+    ship.health -= 1
   end
 
   def render(show = false)
-    #add optional argument which reveals hidden ships
-    if fired_upon == false
+    if @fired_upon == false
       if show == true && empty? == false
         "S"
       else
         "."
       end
-    elsif fired_upon == true && ship.health == ship.length
+    elsif @fired_upon == true && ship.health == ship.length
       "M"
-    elsif fired_upon == true && ship.length > ship.health < 0
-      # not sure if this combination of conditionals will work 
+    elsif @fired_upon == true && ship.length > ship.health < 0
+      # not sure if this combination of conditionals will work
       "H"
-    elsif fired_upon == true && ship.sunk? == true
+    elsif @fired_upon == true && ship.sunk? == true
       "X"
     end
+  end
+end
     # it takes an optional boolean argument but I don't understand why
     # this method displays whether the cell has been fired upon, whether it contains a ship, etc.
     #
-  end
-end
