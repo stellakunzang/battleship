77e887f5242a071ba77810c54521e000e821ba9c
Jack Gould <johnhilsmangould@gmail.com>
2020-03-25 16:10:02 -0600

    add test_it_has_valid_coordinates test method for true and false cells

--- test/board_test.rb
+++ test/board_test.rb
@@ -18,4 +18,11 @@ class BoardTest < Minitest::Test
 
     assert_equal 16, board.cells.length
   end
+
+  def test_it_has_valid_coordinates
+    board = Board.new
+
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal false, board.valid_coordinate?("E5")
+  end
 end
