2f2c9584bb77efab9f4be990ea3d892251cc3851
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-25 13:01:06 -0600

    Created cells method

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,31 @@
 class Board :cells
 
   def initialize
-    @cells
+    @cells = cells
   end
 
-  # cells is a hash with the coordinate (a string with letter and number) as key and value is an actual cell 
+  def cells
+    cells = {}
+    x_axis = ("A".."D").to_a
+    y_axis = ("1".."4").to_a
+
+    keys = []
+    x_axis.each do |x|
+      y_axis.each do |y|
+        keys << (x + y)
+      end
+    end
+
+    values = []
+    keys.each do |key|
+      values << Cell.new(key)
+    end
+
+    keys.each do |key|
+      values.each do |value|
+        cells[key] = value
+      end
+    end
+    cells
+  end
 end
