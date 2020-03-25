3a99e54e5517022a63781e561d364f5569c8b375
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-25 11:48:54 -0600

    Add test_it_exists

---
+++ test/board_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require 'pry'
+
+class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new
+
+    assert_instance_of Board, board
+  end
+
+  def test_it_has_attributes
+
+  end
+end 
