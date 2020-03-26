3e2308fbe5f5c0669a65a94eb0a1e1660fabb30a
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-25 19:21:46 -0600

    Add placeholder tests for render method variations

--- test/board_test.rb
+++ test/board_test.rb
@@ -16,6 +16,36 @@ class BoardTest < Minitest::Test
   def test_it_has_attributes
     board = Board.new
 
-    assert_equal 16, board.cells.length
+    # asserting length is problematic because it can fluctuate; need to test hash itself
+  end
+
+  def test_it_can_render_default
+    board = Board.new
+    board.render
+    # use string methods to test that the basic board at start of game is...
+
+  end
+
+  def test_it_can_render_show_true
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.render(true)
+
+  end
+
+  def test_it_can_render_with_hits
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+    board.render
+
+  end
+
+  def test_it_can_render_sunken_ships
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 1)
+    cruiser.hit
+    board.render
+
   end
 end
