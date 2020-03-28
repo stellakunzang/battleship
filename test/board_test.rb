5febeff9b0e165130070028e79554be1e4d7efc6
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-27 21:05:29 -0600

    Add tests for difference render scenarios

--- test/board_test.rb
+++ test/board_test.rb
@@ -36,6 +36,7 @@ class BoardTest < Minitest::Test
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
+
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
     assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
   end
@@ -44,6 +45,7 @@ class BoardTest < Minitest::Test
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
+
     assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
     assert_equal true, board.valid_placement?(cruiser, ["A1", "B1", "C1"])
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
@@ -54,18 +56,21 @@ class BoardTest < Minitest::Test
 
   def test_it_can_determine_validity_of_coordinate_array
     board = Board.new
+
     assert_equal true, board.valid_coordinate_array?(["A1", "A2", "A3"])
     assert_equal false, board.valid_coordinate_array?(["A1", "A2", "A4"])
   end
 
   def test_it_can_determine_if_letters_are_consecutive
     board = Board.new
+
     assert_equal true, board.consecutive_letters?(["A1", "B1", "C1"])
     assert_equal false, board.consecutive_letters?(["A1", "A1", "C1"])
   end
 
   def test_it_can_determine_if_numbers_are_consecutive
     board = Board.new
+
     assert_equal true, board.consecutive_numbers?(["A1", "A2", "A3"])
     assert_equal false, board.consecutive_numbers?(["A1", "A2", "A4"])
   end
@@ -74,45 +79,74 @@ class BoardTest < Minitest::Test
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
+
     assert_equal false, board.valid_placement?(cruiser, ["A1", "B2", "C3"])
     assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
   end
 
-  def test_it_can_render_default
-    skip
+  def test_it_can_invalidate_overlapping_ships
     board = Board.new
-    board.render
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    submarine = Ship.new("Submarine", 2)
+    board.place(submarine, ["A3, B3"])
 
-    # use string methods to test that the basic board at start of game is...
-    #since it will always start 1234 A A1 can we test that?
+    assert_equal false, board.valid_placement
+  end
 
+  def test_it_can_render_default
+    board = Board.new
+    cell1 = board.cells["A1"]
+    cell2 = board.cells["A2"]
+    cell3 = board.cells["A3"]
+
+    assert_equal ".", cell1.render
+    assert_equal ".", cell2.render
+    assert_equal ".", cell3.render
   end
 
   def test_it_can_render_show_true
-    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
-    board.render(true)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell1 = board.cells["A1"]
+    cell2 = board.cells["A2"]
+    cell3 = board.cells["A3"]
 
+    assert_equal ".", cell1.render
+    assert_equal ".", cell2.render
+    assert_equal ".", cell3.render
+
+    assert_equal "S", cell1.render(true)
+    assert_equal "S", cell2.render(true)
+    assert_equal "S", cell3.render(true)
   end
 
-  def test_it_can_render_with_hits
-    skip
+  def test_it_can_render_hits_and_misses
     board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    cruiser.hit
-    board.render
+    cruiser = Ship.new("Cruiser", 2)
+    board.place(cruiser, ["A1", "A2"])
+    cell1 = board.cells["A1"]
+    cell2 = board.cells["A2"]
+    cell3 = board.cells["A3"]
+    cell2.fire_upon
+    cell3.fire_upon
 
+    assert_equal ".", cell1.render
+    assert_equal "H", cell2.render
+    assert_equal "M", cell3.render
   end
-
-  #write tests for H and X
 
   def test_it_can_render_sunken_ships
-    skip
     board = Board.new
-    cruiser = Ship.new("Cruiser", 1)
-    cruiser.hit
-    board.render
+    cruiser = Ship.new("Cruiser", 2)
+    board.place(cruiser, ["A1", "A2"])
+    cell1 = board.cells["A1"]
+    cell2 = board.cells["A2"]
+    cell1.fire_upon
+    cell2.fire_upon
 
+    assert_equal "X", cell1.render
+    assert_equal "X", cell2.render
   end
 end
