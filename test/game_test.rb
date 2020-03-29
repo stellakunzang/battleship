a70a594b2dff2a2e749697ef8ec7d5b8236644a9
Jack Gould <johnhilsmangould@gmail.com>
2020-03-29 13:55:08 -0600

    add tests for game class and methods

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,1 +1,38 @@
-#game test file 
+#game test file
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+require 'pry'
+
+
+class GameTest < Minitest::Test
+
+  def test_it_exists
+    game = Game.new('player1')
+
+    assert_instance_of Game, game
+  end
+
+  def test_it_has_readable_attributes
+
+    game = Game.new('player1')
+
+    assert_equal 'player1', game.player
+    assert_equal [], game.coordinates
+  end
+
+
+  def test_it_can_create_coordinates
+    skip
+    board = Board.new
+    board.create_coordinates_array
+
+    assert_equal ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"], board.create_coordinates_array
+  end
+
+
+
+end
