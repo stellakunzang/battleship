51b2817731967a08a7a5d4f6c4d998b1d9a0ec97
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-31 21:41:42 -0600

    Fixed user_input within setup to handle variety of input

--- lib/game.rb
+++ lib/game.rb
@@ -33,7 +33,7 @@ class Game
     @player_board.render
     "Enter the squares for the Cruiser (3 spaces): "
     user_input = gets.chomp
-    coordinates = user_input.upcase.gsub(/[^[:word:]\s]/,'').scan(/../)
+    coordinates = user_input.upcase.gsub(/[^\w]\s+/,'').scan(/../)
     validate_user_placement_cruiser(cruiser, coordinates)
   end
 
@@ -43,12 +43,12 @@ class Game
       @player_board.render(true)
       puts "Enter the squares for the Submarine (2 spaces): "
       user_input = gets.chomp
-      coordinates = user_input.upcase.gsub(/[^[:word:]\s]/,'').scan(/../)
+      coordinates = user_input.upcase.gsub(/[^\w]\s+/,'').scan(/../)
       validate_user_placement_submarine(submarine, coordinates)
     else
       puts "Those are invalid coordiantes. Please try again: "
       user_input = gets.chomp
-      coordinates = user_input.upcase.gsub(/[^[:word:]\s]/,'').scan(/../)
+      coordinates = user_input.upcase.gsub(/[^\w]\s+/,'').scan(/../)
       validate_user_placement_cruiser(cruiser, coordinates)
     end
   end
@@ -61,7 +61,7 @@ class Game
     else
       puts "Those are invalid coordiantes. Please try again: "
       user_input = gets.chomp
-      coordinates = user_input.upcase.gsub(/[^[:word:]\s]/,'').scan(/../)
+      coordinates = user_input.upcase.gsub(/[^\w]\s+/,'').scan(/../)
       validate_user_placement_submarine(submarine, coordinates)
     end
   end
