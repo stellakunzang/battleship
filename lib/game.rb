6ecca4890693ff2e941fdc99caeafd89f1582c6e
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-31 21:24:13 -0600

    Moved turn methods to game

--- lib/game.rb
+++ lib/game.rb
@@ -1,151 +1,157 @@
-#game class file
-require './lib/cell'
-require './lib/board'
-require './lib/ship'
+class Game
+  attr_reader :cruiser, :submarine, :computer, :player_cells_fired_upon
+  attr_accessor :player_board
 
- class Game
-
-  attr_reader :player, :cruiser, :submarine
-  attr_accessor :computer_board
-
-  def initialize(player)
-    @player = player
-    @computer_board = Board.new
-    @player_board = Board.new
+  def initialize
+    @player_board ||= Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
-
+    @computer = Computer.new
+    @player_cells_fired_upon = []
   end
 
-  def user_places_ships(ship, coordinates)
-    loop do
-      p "which ship would you like to place? => "
-        ship = gets.chomp
-      p "enter coordinates => "
-        coordinates = gets.chomp
-      if @player_board.valid_placement?(ship, coordinates)
-          @player_board.place(ship, coordinates)
-          break
-      else
-        p "Those are invalid cooridnates. Please try again!"
-      end
-    end
-  end
-
-  def random_coordinates_cruiser
-    random_number = rand(1..10)
-
-    if random_number.even?
-      coordinates_array = []
-      x_axis = ("A".."D").to_a
-      x_axis = x_axis[rand(0..3)]
-      coord = x_axis
-
-      y_axis = ("1".."4").to_a
-      y_axis.each do |y|
-          coordinates_array << coord + y
-        end
-
-          if random_number.even?
-            coordinates_array.pop
-          else
-            coordinates_array.shift
-          end
+  def main_menu
+    puts "Welcome to BATTLESHIP \n" +
+    "Enter p to play. Enter q to quit. \n"
+    reply = gets.chomp
+    if reply == "p"
+      setup
+    elsif
+      reply == "q"
+      puts "Goodbye"
     else
-      coordinates_array = []
-      y_axis = ("1".."4").to_a
-      y_axis = y_axis[rand(0..3)]
-      coord = y_axis
-
-      x_axis = ("A".."D").to_a
-      x_axis.each do |x|
-          coordinates_array << x + coord
-        end
-          if random_number.even?
-            coordinates_array.pop
-          else
-            coordinates_array.shift
-          end
+      main_menu
     end
-      coordinates_array
   end
 
-  def random_coordinates_submarine
-    random_number = rand(1..30)
-    range1 = 1..10
-    range2 = 11..20
+  def setup
+    computer.place_cruiser
+    computer.place_submarine
+    puts "I have laid out my ships on the grip. \n"+
+    "You now need to lay out your two ships. \n"+
+    "The Cruiser is three units long and the Submarine is two units long. \n"
+    @player_board.render
+    "Enter the squares for the Cruiser (3 spaces): "
+    user_input = gets.chomp
+    coordinates = user_input.upcase.gsub(/[^[:word:]\s]/,'').scan(/../)
+    validate_user_placement_cruiser(cruiser, coordinates)
+  end
 
-    if random_number.even?
-      coordinates_array = []
-      x_axis = ("A".."D").to_a
-      x_axis = x_axis[rand(0..3)]
-      coord = x_axis
-
-      y_axis = ("1".."4").to_a
-      y_axis.each do |y|
-          coordinates_array << coord + y
-        end
-
-          if range1.include?(random_number)
-            coordinates_array.pop(2)
-          elsif range2.include?(random_number)
-            coordinates_array.shift
-            coordinates_array.shift
-          else
-            coordinates_array.shift
-            coordinates_array.pop
-          end
-        coordinates_array
+  def validate_user_placement_cruiser(cruiser, coordinates)
+    if @player_board.valid_placement?(cruiser, coordinates)
+      @player_board.place(cruiser, coordinates)
+      @player_board.render(true)
+      puts "Enter the squares for the Submarine (2 spaces): "
+      user_input = gets.chomp
+      coordinates = user_input.upcase.gsub(/[^[:word:]\s]/,'').scan(/../)
+      validate_user_placement_submarine(submarine, coordinates)
     else
-      coordinates_array = []
-      y_axis = ("1".."4").to_a
-      y_axis = y_axis[rand(0..3)]
-      coord = y_axis
-
-      x_axis = ("A".."D").to_a
-      x_axis.each do |x|
-          coordinates_array << x + coord
-        end
-        if range1.include?(random_number)
-          coordinates_array.pop(2)
-        elsif range2.include?(random_number)
-          coordinates_array.shift
-          coordinates_array.shift
-        else
-          coordinates_array.shift
-          coordinates_array.pop
-        end
-        coordinates_array
+      puts "Those are invalid coordiantes. Please try again: "
+      user_input = gets.chomp
+      coordinates = user_input.upcase.gsub(/[^[:word:]\s]/,'').scan(/../)
+      validate_user_placement_cruiser(cruiser, coordinates)
     end
-
   end
 
-  def place_cruiser
-    @computer_board.place(cruiser, random_coordinates_cruiser)
-  end
-
-  def sub_coordinates_are_valid
-    #put counter to make sure that loop was running.
-    counter = 0
-    loop do
-      if @computer_board.valid_placement?(submarine, random_coordinates_submarine) == true
-        break
-      else
-        counter += 1
-      end
-
+  def validate_user_placement_submarine(submarine, coordinates)
+    if @player_board.valid_placement?(submarine, coordinates)
+      @player_board.place(submarine, coordinates)
+      @player_board.render(true)
+      initiate_game
+    else
+      puts "Those are invalid coordiantes. Please try again: "
+      user_input = gets.chomp
+      coordinates = user_input.upcase.gsub(/[^[:word:]\s]/,'').scan(/../)
+      validate_user_placement_submarine(submarine, coordinates)
     end
-    #if im correct this gets returned when the method lands on valid coordiantes
-    true
   end
 
-  def place_submarine
-      #how can we test this?
-    if sub_coordinates_are_valid == true
-        @computer_board.place(submarine, random_coordinates_submarine)
+  def initiate_game
+    commence_player_turn
+  end
+
+  def commence_player_turn
+    puts "============Computer Board============"
+    puts computer.board.render
+    puts "============Player Board============"
+    puts player_board.render(true)
+    puts "~*~*~*~*~*~*~Take A Turn~*~*~*~*~*~*~"
+    get_coordinate
+  end
+
+  def get_coordinate
+    puts "Enter the coordinate for your shot: "
+    coordinate = gets.chomp.upcase
+    validate_coordinate(coordinate)
+  end
+
+  def validate_coordinate(coordinate)
+    if @player_cells_fired_upon.include?(coordinate)
+      warn_player_repeat_target
+    elsif player_board.valid_coordinate?(coordinate) == false
+      puts "Please enter a valid coordinate: "
+      coordinate = gets.chomp.upcase
+      validate_coordinate(coordinate)
+    elsif player_board.valid_coordinate?(coordinate) == true
+      player_turn(coordinate)
     end
-
   end
 
+  def warn_player_repeat_target
+    puts "You have already fired on that cell. Try again."
+    get_coordinate
+  end
 
+  def player_turn(coordinate)
+    computer.board.cells[coordinate].fire_upon
+    @player_cells_fired_upon << coordinate
+    report_player_turn_results(coordinate)
+    computer_turn
+  end
+
+  def computer_turn
+    coordinate = computer.computer_target_coordinates.first
+    player_board.cells[coordinate].fire_upon
+    computer.computer_target_coordinates.shift
+    report_computer_turn_results(coordinate)
+    end_game?
+  end
+
+  def end_game?
+    if @cruiser.sunk? == true && @submarine.sunk? == true
+      puts "I won!"
+    elsif computer.cruiser.sunk? == true && computer.submarine.sunk? == true
+      puts "You won!"
+    else
+      commence_player_turn
+    end
+  end
+
+  def report_player_turn_results(coordinate)
+    puts "Your shot on #{coordinate} #{player_turn_result(coordinate)}."
+  end
+
+  def report_computer_turn_results(coordinate)
+    puts "My shot on #{coordinate} #{computer_turn_result(coordinate)}."
+  end
+
+  def computer_turn_result(coordinate)
+    if player_board.cells[coordinate].empty == true
+      "was a miss"
+    elsif player_board.cells[coordinate].render == "H"
+      "hit a ship"
+    elsif player_board.cells[coordinate].render == "X"
+      "sunk a ship"
+    end
+  end
+
+  def player_turn_result(coordinate)
+    if computer.board.cells[coordinate].empty == true
+      "was a miss"
+    elsif computer.board.cells[coordinate].render == "H"
+      "hit a ship"
+    elsif computer.board.cells[coordinate].render == "X"
+      "sunk a ship"
+    end
+  end
 end
